3c3998b1e8585dc74addb503adfa9a5d155b5e44
theodoredyer <tdyer@ucsc.edu>
2020-07-15 21:25:39 -0700

    async start

--- WDS-notes/script.js
+++ WDS-notes/script.js
@@ -1,14 +1,42 @@
-let p = new Promise((resolve, reject) => {
-    let a = 1 + 1
-    if(a == 2) {
-        resolve('Success')
-    } else {
-        reject('Failed')
-    }
+function makeRequest(location) {
+    return new Promise((resolve, reject) => {
+        console.log(`Making Request to + ${location}`)
+        if(location === 'Google') {
+            resolve('Google says hi')
+        } else {
+            reject('We can only talk to googs')
+        }
+    })
+}
+
+function processRequest(response) {
+    return new Promise((resolve, reject) => {
+        console.log('Processing response')
+        resolve(`Extra Information + ${response}`)
+    })
+}
+
+makeRequest('Google').then(respone => {
+    console.log('Response Received')
+    return processRequest(response)
+}).then(processedResponse => {
+    console.log(processedResponse)
+}).catch(err => {
+    console.log(err)
 })
 
-p.then((message) => {
-    console.log('in the then + ' + message)
-}).catch((message) => {
-    console.log('in catch + ' + message)
-})
+
+// let p = new Promise((resolve, reject) => {
+//     let a = 1 + 1
+//     if(a == 2) {
+//         resolve('Success')
+//     } else {
+//         reject('Failed')
+//     }
+// })
+
+// p.then((message) => {
+//     console.log('in the then + ' + message)
+// }).catch((message) => {
+//     console.log('in catch + ' + message)
+// })
