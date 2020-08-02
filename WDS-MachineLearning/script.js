219e0f6bd58c1b75c3f351382c4275d785ba2ca3
theodoredyer <tdyer@ucsc.edu>
2020-08-01 21:45:12 -0700

    updates

--- WDS-MachineLearning/script.js
+++ WDS-MachineLearning/script.js
@@ -1,23 +1,32 @@
 const net = new brain.NeuralNetwork()
 
-net.train([
+const data = [
     {
-        input: [0,0],
-        output: [0]
-    },
-    {
-        input: [1,0],
+        input: { r: 0, g: 0, b: 0 },
         output: [1]
     },
     {
-        input: [0,1],
-        output: [1]
-    },
-    {
-        input: [1,1],
+        input: { r: 1, g: 1, b: 1 },
         output: [0]
     }
-])
+]
+
+net.train(data)
 
 const diagram = document.getElementById('diagram')
 diagram.innerHTML = brain.utilities.toSVG(net)
+
+const colorEl = document.getElementById('color')
+const guessEl = document.getElementById('guess')
+const whiteBtn = document.getElementById('white-btn')
+const blackBtn = document.getElementById('black-btn')
+const printBtn = document.getElementById('print-btn')
+setRandomColor()
+
+function setRandomColor() {
+    color = {
+        r: Math.random(),
+        g: Math.random(),
+        b: Math.random()
+    }
+}
